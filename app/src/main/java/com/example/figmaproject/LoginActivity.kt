ffa65286fb5e6d63ec54841effcdf1e1ddcf67c1
VaibhavJonty <vaibhav.purohit10@gmail.com>
2024-05-31 15:42:14 +0530

    changes done

--- app/src/main/java/com/example/figmaproject/LoginActivity.kt
+++ app/src/main/java/com/example/figmaproject/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.example.figmaproject
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -21,8 +22,44 @@ class LoginActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        binding.button.setOnClickListener {
-            startActivity(Intent(this, FeedActivity::class.java))
+        binding.btnLogin.setOnClickListener {
+            val email = binding.editEmail.text
+            val password = binding.editPassword.text
+            if (email.isEmpty()) {
+                Toast.makeText(this, "Please enter your email", Toast.LENGTH_SHORT).show()
+            } else if (!isValidEmail(email.toString())) {
+                Toast.makeText(this, "Please enter a valid email", Toast.LENGTH_SHORT).show()
+            } else if (password.isEmpty()) {
+                Toast.makeText(this, "Please enter your password", Toast.LENGTH_SHORT).show()
+            }else if(!validatePassword(password.toString())) {
+                Toast.makeText(this, "Please enter a valid password", Toast.LENGTH_SHORT).show()
+            }else {
+                startActivity(Intent(this, FeedActivity::class.java))
+            }
+
         }
     }
+
+    fun isValidEmail(email: String): Boolean {
+        val emailRegex = Regex("^[A-Za-z0-9+_.-]+@(.+)$")
+        return emailRegex.matches(email)
+    }
+
+    fun validatePassword(password: String): Boolean {
+        // Define the password validation rules
+        val minLength = 8
+        val maxLength = 20
+        val hasUppercase = password.any { it.isUpperCase() }
+        val hasLowercase = password.any { it.isLowerCase() }
+        val hasDigit = password.any { it.isDigit() }
+        val hasSpecialChar = password.any { !it.isLetterOrDigit() }
+
+        // Check the rules
+        val isValidLength = password.length in minLength..maxLength
+        val isValid = isValidLength && hasUppercase && hasLowercase && hasDigit && hasSpecialChar
+
+        // Return the validation result
+        return isValid
+    }
+
 }
